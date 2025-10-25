f1060e2d7449f4fcda4cb311da7caf233a5e82b3
mwaqasyaqoob21 <mwaqasyaqoob21@gmail.com>
2025-10-25 15:04:48 -0400

    Update viral-channel.py

--- viral-channel.py
+++ viral-channel.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "Enter your API Key here"
+API_KEY = "AIzaSyDJcigV37FRMhkO73M97OUm85tb82y6HM0"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
